f47abd309e906320d845e3a737d5e2cc7f623787
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-09-05 05:39:57 +0400

    Started CS193p

---
+++ CS196p/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vakhtang Saginadze on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
