6ae73a673b3994269227e0e0742037ffc488a9ca
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-01 04:20:22 +0400

    read-only computed properties

--- CS193p/reading-assignment-2.swift
+++ CS193p/reading-assignment-2.swift
@@ -132,3 +132,12 @@ var initialSquareCenter = square.center
 square.center = Point(x: 15.0, y: 15.0)
 print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
  
+
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
