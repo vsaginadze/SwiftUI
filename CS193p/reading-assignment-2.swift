ba47ce3bc530a5429437964fcaf28a73c3ac097c
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-01 08:20:15 +0400

    extensions

--- CS193p/reading-assignment-2.swift
+++ CS193p/reading-assignment-2.swift
@@ -410,3 +410,35 @@ extension Rect {
         self.init(origin: Point(x: originX, y: originY), size: size)
     }
 }
+
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+func printIntegerKinds(_ numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case .negative:
+            print("- ", terminator: "")
+        case .zero:
+            print("0 ", terminator: "")
+        case .positive:
+            print("+ ", terminator: "")
+        }
+    }
+    print("")
+}
+printIntegerKinds([3, 19, -27, 0, -6, 0, 7])
+
