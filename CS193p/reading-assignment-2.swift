c8373f4aa62aa2c88c3c4b01b8aeebbcbf99fc3e
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-01 04:17:08 +0400

    shorthand declarations for setter and getter

--- CS193p/reading-assignment-2.swift
+++ CS193p/reading-assignment-2.swift
@@ -118,14 +118,11 @@ struct Rectangle {
     
     var center: Point {
         get {
-            let centerX = origin.x + (size.width / 2)
-            let centerY = origin.y + (size.height / 2)
-            
-            return Point(x: centerX, y: centerY)
+            Point(x: origin.x + (size.width / 2), y: origin.y + (size.height / 2))
         }
-        set(newCenter) {
-            origin.x = newCenter.x - (size.width / 2)
-            origin.y = newCenter.y - (size.height / 2)
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
         }
     }
 }
