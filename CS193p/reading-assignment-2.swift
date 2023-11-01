3752a2ba53fa89a63d6a40ad7df4ceb67b494ca2
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-01 07:20:06 +0400

    advanced optional topics

--- CS193p/reading-assignment-2.swift
+++ CS193p/reading-assignment-2.swift
@@ -312,3 +312,70 @@ class Address {
     }
     
 }
+
+
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var rooms: [Room] = []
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
+
+class Room {
+    let name: String
+    init(name: String) { self.name = name }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+        return "#\(buildingNumber), \(street) str."
+        } else if buildingName != nil {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
+}
+
+let john = Person()
+let johnsHouse = Residence()
+johnsHouse.rooms.append(Room(name: "Living Room"))
+johnsHouse.rooms.append(Room(name: "Kitchen"))
+john.residence = johnsHouse
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s)")
+} else {
+    print("Unable to retrieve the number of rooms")
+}
+
+let someAddress = Address()
+someAddress.buildingNumber = "29"
+someAddress.street = "Acacia Road"
+
+if (john.residence?.address = someAddress) != nil {
+    print("It was possible to set an address.")
+} else {
+    print("It was not possible to set an address")
+}
