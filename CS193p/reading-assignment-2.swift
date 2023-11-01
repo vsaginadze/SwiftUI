c2122287cf962949cf3e88e620abb2dcd4a7e02d
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-01 05:00:19 +0400

    initialization parameters

--- CS193p/reading-assignment-2.swift
+++ CS193p/reading-assignment-2.swift
@@ -174,3 +174,20 @@ struct Fahrenheit {
 var f = Fahrenheit()
 print("The default temperature is \(f.temperature)° Fahrenheit")
 // Prints "The default temperature is 32.0° Fahrenheit"
+
+struct Celsius {
+    var temperatureInCelsius: Double
+    
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+
+print(boilingPointOfWater.temperatureInCelsius)
