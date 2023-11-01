30447de360b0595797d268ca252bd8739ce4be4a
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-01 08:18:55 +0400

    extensions

--- CS193p/reading-assignment-2.swift
+++ CS193p/reading-assignment-2.swift
@@ -379,3 +379,34 @@ if (john.residence?.address = someAddress) != nil {
 } else {
     print("It was not possible to set an address")
 }
+
+extension Double {
+    var km: Double { self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0),
+    size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
