68e3bb86b6d83023dbe360ee0e8f0edac705fc0b
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-01 06:43:20 +0400

    defining model classes for optional chaining

--- CS193p/reading-assignment-2.swift
+++ CS193p/reading-assignment-2.swift
@@ -267,3 +267,48 @@ if let roomCount = john.residence?.numberOfRooms {
 } else {
     print("Unable to retrieve the number of rooms.")
 }
+
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var rooms: [Room] = []
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
+
+class Room {
+    let name: String
+    init(name: String) { self.name = name }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+        return "#\(buildingNumber), \(street) str."
+    } else if buildingName != nil {
+        return buildingName
+    } else {
+        return nil
+    }
+    }
+    
+}
