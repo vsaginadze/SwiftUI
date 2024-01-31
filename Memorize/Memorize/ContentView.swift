13199f71392f61622d8f9b4c6124cbb8f2fa4217
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-31 17:14:41 +0400

    changed foregroundColor with fill

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -26,14 +26,11 @@ struct CardView: View {
     var body: some View {
         ZStack {
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("👻")
-                    .font(.largeTitle)
+                RoundedRectangle(cornerRadius: 12).fill(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                RoundedRectangle(cornerRadius: 12).fill()
             }
         }
     }
