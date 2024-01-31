6ab308c6ed70dd9f58199e2b1aa5eeeeed1a66a3
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-31 20:25:57 +0400

    added content to a card

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
+            CardView(content: "👻", isFaceUp: true)
+            CardView(content: "👹")
+            CardView(content: "🕷️", isFaceUp: true)
+            CardView(content: "🎃", isFaceUp: true)
         }
         .foregroundColor(.orange)
         .padding()
@@ -21,6 +21,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = false
     
     var body: some View {
@@ -29,7 +30,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
