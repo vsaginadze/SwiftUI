bd5c526dba83b64d7a52747addb7e69fc2cff28d
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-03 00:07:44 +0400

    added cardCountsAdjuster function and disabled view modifier

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -39,24 +39,21 @@ struct ContentView: View {
         .font(.largeTitle)
     }
     
-    var cardRemover: some View {
+    func cardCountAdjusters(by offset: Int, symbol: String) -> some View {
         Button(action: {
-            if cardCount > 1 {
-                cardCount -= 1
-            }
+            cardCount += offset
         }, label: {
-            Image(systemName: "rectangle.stack.fill.badge.minus")
+            Image(systemName: symbol)
         })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjusters(by: -1, symbol: "rectangle.stack.fill.badge.minus")
     }
     
     var cardAdder: some View {
-        Button(action: {
-            if cardCount < emojis.count {
-                cardCount += 1
-            }
-        }, label: {
-            Image(systemName: "rectangle.stack.fill.badge.plus")
-        })
+        cardCountAdjusters(by: 1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
