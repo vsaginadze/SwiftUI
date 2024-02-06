862a2fb2fe19c5a9e0fdc657f34359b82c2d8fa5
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-06 15:01:26 +0400

    created aspectVGrid

---
+++ Memorize/Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Vakhtang Saginadze on 06.02.2024.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
