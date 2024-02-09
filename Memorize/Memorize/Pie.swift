a4ee750ae8265b04fea7a20a205f9e6c6a518fe6
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-09 21:00:58 +0400

    added PIe animation

--- Memorize/Memorize/Pie.swift
+++ Memorize/Memorize/Pie.swift
@@ -5,4 +5,40 @@
 //  Created by Vakhtang Saginadze on 06.02.2024.
 //
 
-import Foundation
+import SwiftUI
+import CoreGraphics
+
+struct Pie: Shape {
+    var startAngle: Angle = .zero
+    let endAngle: Angle
+    var clockwise = true
+    
+    func path(in rect: CGRect) -> Path {
+        let startAngle = startAngle - .degrees(90)
+        let endAngle = endAngle - .degrees(90)
+        
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * cos(startAngle.radians),
+            y: center.y + radius * sin(startAngle.radians)
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: !clockwise
+        )
+        p.addLine(to: center)
+        
+        
+        return p
+    }
+    
+    
+}
