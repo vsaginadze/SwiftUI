51bad7fc0c3b7afe24bd21510783f09cb1e90a11
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-09 23:16:55 +0400

    created a FlyingNumber

--- Memorize/Memorize/FlyingNumber.swift
+++ Memorize/Memorize/FlyingNumber.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 struct FlyingNumber: View {
     typealias Card = MemoryGame<String>.Card
-    
+    @State private var offset: CGFloat = 0
     let number:  Int
     var body: some View {
         if number != 0 {
-            Text(number, format: .number)
+            Text(number, format: .number.sign(strategy: .always()))
+                .font(.largeTitle)
+                .foregroundStyle(number < 0 ? .red : .green)
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 1.5, x: 1, y: 1)
+                .fontWeight(.semibold)
+                .opacity(offset != 0 ? 0 : 1)
+                .offset(x: 0, y: offset)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 0.6)) {
+                        offset = number < 0 ? 200 : -200
+                    }
+                }
+                .onDisappear {
+                    offset = 0
+                }
         }
     }
 }
