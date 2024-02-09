41b8bc2e8b8e66ca0ab1f313fc6f7b1955a32cd0
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-09 21:02:28 +0400

    added scoring to view

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -10,86 +10,51 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
+    private let aspectRatio: CGFloat = 2/3
+    private let spacing: CGFloat = 4
+    
     var body: some View {
         VStack {
             cards
-                .animation(.default, value: viewModel.cards)
+                .foregroundColor(viewModel.color)
             
-            Button("Shuffle") {
-                viewModel.shuffle()
+            HStack {
+                score
+                
+                Spacer()
+                
+                shuffleButton
             }
+            .font(.title2)
+            .fontWeight(.medium)
         }
         .padding()
     }
     
-    var cards: some View {
-        GeometryReader { geometry in
-            
-            let gridItemSize = gridItemWidthThatFits(
-                count: viewModel.cards.count,
-                size: geometry.size,
-                atAspectRatio: 2/3
-            )
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                ForEach(viewModel.cards) { card in
-                    CardView(card)
-                        .aspectRatio(2/3, contentMode: .fit)
-                        .padding(4)
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
+    private var score: some View {
+        Text("Score: \(viewModel.score)")
+            .animation(nil)
+    }
+    
+    private var shuffleButton: some View {
+        Button("Shuffle") {
+            withAnimation {
+                viewModel.shuffle()
+            }
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var cards: some View {
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
+            CardView(card)
+                .padding(spacing)
+                .onTapGesture {
+                    withAnimation(.easeInOut(duration: 3)) {
+                        viewModel.choose(card)
+                    }
                 }
-            }
         }
-        .foregroundColor(.orange)
-    }
-    
-    func gridItemWidthThatFits(
-        count: Int,
-        size: CGSize,
-        atAspectRatio aspectRatio: CGFloat
-    ) -> CGFloat {
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            
-            let rowCount = (count / columnCount).rounded(.up)
-            if rowCount * height < size.height {
-                return (size.width / columnCount).rounded(.down)
-            }
-            columnCount += 1
-        } while columnCount < count
-        
-        return min(size.width / count, size.height * aspectRatio).rounded(.down)
-    }
-}
-
-struct CardView: View {
-    let card: MemoryGame<String>.Card
-    
-    init(_ card: MemoryGame<String>.Card) {
-        self.card = card
-    }
-    
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(card.content)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.01)
-                    .aspectRatio(contentMode: .fit)
-            }
-            .opacity(card.isFaceUp ? 1 : 0)
-            
-            base.fill().opacity(card.isFaceUp ? 0 : 1)
-            
-        }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
